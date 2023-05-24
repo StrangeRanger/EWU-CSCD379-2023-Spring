391e80a7c7e500fc6d59397a2072c921da7b5852
Hunter T <strangeranger01@randomserver.xyz>
2023-05-23 21:42:54 -0700

    Api upgrade - part 4

--- Wordle.Api/Wordle.Api/Controllers/WordController.cs
+++ Wordle.Api/Wordle.Api/Controllers/WordController.cs
@@ -1,6 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Wordle.Api.Data;
+﻿using Microsoft.AspNetCore.Mvc;
 using Wordle.Api.Dtos;
 using Wordle.Api.Services;
 
@@ -20,31 +18,24 @@ public class WordController : ControllerBase
     [HttpGet]
     public async Task<string> Get()
     {
-        return await _wordService.GetRandomWord();
+        return (await _wordService.GetRandomWordAsync()).Text;
     }
 
-    // NOTE: Disabled because unused. Respective service method and UI components are also disabled.
-    //       (Disabled group set => Set 1)
-    // [HttpGet("GetManyWords")]
-    // public async Task<IEnumerable<Word>> GetManyWords(int? count)
+    // [HttpGet("WordOfTheDay")]
+    // public async Task<WordOfTheDayDto> GetWordOfTheDay(double offsetInHours, DateTime? date =
+    // null)
     // {
-    //     return await _wordService.GetSeveralWords(count);
-    // }
-    
-    // NOTE: Disabled because unused. Respective service method and UI components are also disabled.
-    //       (Disabled group set => Set 1)
-    // [HttpPost]
-    // public async Task<Word> AddWord(string newWord, bool isCommon)
-    // {
-    //     return await _wordService.AddWord(newWord, isCommon);
+    //     return new WordOfTheDayDto(
+    //         await _wordService.GetWordOfTheDayAsync(TimeSpan.FromHours(offsetInHours), date));
     // }
 
-    // NOTE: Disabled because unused. Respective service method and UI components are also disabled.
-    //       (Disabled group set => Set 1)
-    // [HttpPost("AddWordFromBody")]
-    // public async Task<Word> AddWordFromBody([FromBody] WordDto word)
+    // [HttpGet("WordOfTheDayStats")]
+    // public async Task<IEnumerable<WordOfTheDayStatsDto>> GetWordOfTheDayStats(DateTime? date =
+    // null,
+    //     int days = 10,
+    //     Guid? playerId = null)
     // {
-    //     return await _wordService.AddWord(word.Text, word.IsCommon);
+    //     return (await _wordService.GetWordOfTheDayStatsAsync(date, days, playerId));
     // }
 
     [HttpGet("GetWordList")]
